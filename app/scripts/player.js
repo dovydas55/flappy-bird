a3d9968a4a98d15ca39e0d21d451f169fc7d648c
Dovydas <dovydas55@hotmail.com>
2015-04-01 03:29:13 +0000

    added some basic sound support

--- app/scripts/player.js
+++ app/scripts/player.js
@@ -22,6 +22,7 @@ window.Player = (function() {
 
 		this.el = el;
 		this.flapSound = document.getElementById("flapSound");
+		this.dieSound = document.getElementById("dieSound");
 		this.jump = true;
 		this.velocity = 0;
 		this.game = game;
@@ -98,8 +99,8 @@ window.Player = (function() {
 	Player.prototype.flap = function() {
 		console.log('flap');
 		this.velocity -= 2.1;
-		//this.flapSound.load();
-		//this.flapSound.play();
+		this.flapSound.load();
+		this.flapSound.play();
 	};
 
 	Player.prototype.onFrame = function(delta) {
@@ -143,6 +144,8 @@ window.Player = (function() {
 			this.pos.y + HEIGHT > this.game.WORLD_HEIGHT) {
 			notInitialState = false;
 			afterRestart = true;
+
+			this.dieSound.play();
 			return this.game.gameover();
 
 		}
